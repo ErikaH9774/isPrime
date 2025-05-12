7356b93b7e2c5aecdde38aff38021860fddb3154
ErikaH92 <erv27240@email.vccs.edu>
2025-05-12 00:54:38 -0400

    isprime coding.

--- isPrime/isPrime.cpp
+++ isPrime/isPrime.cpp
@@ -2,19 +2,39 @@
 //
 
 #include <iostream>
+//cmath for the sqrt
+#include <cmath>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    //user enters a number and is being asked to enter a number. 
+    int num;
+    cout << "Enter a number: \n ";
+    cin >> num;
+    //if the number is less than 2.
+    if (num < 2) {
+        //display if number entered is less than 2 and is so define if it is not a prime number 
+        cout << num << " is NOT a prime number. \n ";
+    }
+    //define if prime is true 
+    else {
+        bool isPrime = true;
+        //int i = 2 and i is less than or equal to the square root of num and i ++ 
+        for (int i = 2; i <= sqrt(num); i++) {
+            //if num % i == 0 then is prime is false
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        // in summary if isprime of num is a prime number and if is prime of num is not a prime number. 
+        if (isPrime) {
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+            cout << num << " IS a prime number. \n ";
+            cout << num << " is NOT a prime number \n ";
+        }
+    }
+    return 0;
+   
